7db2e7ee0470649e6e857876ccb196c9326253c0
Tidadini <51876470+Tidadini@users.noreply.github.com>
2020-06-01 23:24:07 +0200

    Update HTTP_request.py

--- HTTP_request.py
+++ HTTP_request.py
@@ -25,10 +25,25 @@ with requests.Session() as s:
     list_nodes.json()
     print(list_nodes.json())
 
-    data_node = '{"type":"qemu","template":"vios","config":"Unconfigured","delay":0,"icon":"Router.png","image":"vios-adventerprisek9-m-15.5.3M","name":"Core Router 1","left":"35%","top":"25%","ram":"1024","console":"telnet","cpu":1,"ethernet":2,"uuid":"e967819b-626c-4e5c-afa6-ebb4476bc19e"}'
-    add_node = s.post("http://192.168.43.131/api/labs/Autobot.unl/nodes",
+    data_node1 = '{"type":"qemu","template":"vios","config":"Unconfigured","delay":0,"icon":"Router.png","image":"vios-adventerprisek9-m-15.5.3M","name":"Core Router 1","left":"35%","top":"25%","ram":"1024","console":"telnet","cpu":1,"ethernet":2,"uuid":"e967819b-626c-4e5c-afa6-ebb4476bc19e"}'
+    data_node2 = '{"type":"qemu","template":"vios","config":"Unconfigured","delay":0,"icon":"Router.png","image":"vios-adventerprisek9-m-15.5.3M","name":"Core Router 1","left":"45%","top":"35%","ram":"1024","console":"telnet","cpu":1,"ethernet":2,"uuid":"e967819b-626c-4e5c-afa6-ebb4476bc19e"}'
+    data_node3 = '{"type":"qemu","template":"vios","config":"Unconfigured","delay":0,"icon":"Router.png","image":"vios-adventerprisek9-m-15.5.3M","name":"Core Router 1","left":"55%","top":"45%","ram":"1024","console":"telnet","cpu":1,"ethernet":2,"uuid":"e967819b-626c-4e5c-afa6-ebb4476bc19e"}'
+
+    add_node1 = s.post("http://192.168.43.131/api/labs/Autobot.unl/nodes",
         cookies=cookies,
-        data=data_node,
+        data=data_node1,
         )
-    add_node.json()
-    print(add_node.json())
+    add_node2 = s.post("http://192.168.43.131/api/labs/Autobot.unl/nodes",
+        cookies=cookies,
+        data=data_node2,
+        )
+    add_node3 = s.post("http://192.168.43.131/api/labs/Autobot.unl/nodes",
+        cookies=cookies,
+        data=data_node3,
+        )
+    add_node1.json()
+    print(add_node1.json())
+    add_node2.json()
+    print(add_node2.json())
+    add_node3.json()
+    print(add_node3.json())
